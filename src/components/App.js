7054244f63ab4d00f1fec4582508a693cb8c5285
Amit Bothra <amit.bothra@outlook.com>
2018-12-29 06:42:30 -0600

    create Searchbar and env file for api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render () {
     
     return (
-    <div>
-      App
+    <div className="ui container">
+      < SearchBar />
     </div>
     );
   }
