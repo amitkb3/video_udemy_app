e5dd20852125f22a1ddc1c589cbbb11bd95c0333
Amit Bothra <amit.bothra@outlook.com>
2018-12-30 06:48:39 -0600

    captured selected video via deeply nested callbacks

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 import VideoList from './VideoList';
 import './VideoItem.css';
 
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
 
   return(
-    <div className="video-item item">
+    <div onClick={() =>onVideoSelect(video)} className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
          <div className="header">
