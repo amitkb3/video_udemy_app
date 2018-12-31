f88e8e4b154440c3572ce1687af8f625580b3345
Amit Bothra <amit.bothra@outlook.com>
2018-12-31 06:52:37 -0600

    added type paramenter to youtube api call

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
+//added 'type' parameter to only return videos
+// this removes channel or playlist which 
+// provide no videoid and hence error
+
 const API_KEY = process.env.REACT_APP_API_KEY_YT;
 
 export default  axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part:'snippet',
+    type: 'video',
     maxResults: 5,
     key: API_KEY
 }
